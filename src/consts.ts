16a4ae3497f7bb87662f9a15a02b236b6ec0f984
giyaseddin <giyaseddin@innoem.net>
2026-01-30 05:12:25 +0300

    Update consts

--- src/consts.ts
+++ src/consts.ts
@@ -2,27 +2,26 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "Giyas Bayrak",
+  DESCRIPTION: "Welcome to Giyas's (غياث) website, a portfolio and a personal blog focused on building AI solutions.",  AUTHOR: "Giyaseddin Bayrak",
 }
 
 // Work Page
 export const WORK: Page = {
-  TITLE: "Work",
-  DESCRIPTION: "Places I have worked.",
+  TITLE: "Resume",
+  DESCRIPTION: "My professional Resume.",
 }
 
 // Blog Page
 export const BLOG: Page = {
   TITLE: "Blog",
-  DESCRIPTION: "Writing on topics I am passionate about.",
+  DESCRIPTION: "A narrative of your professional journey and the problems you solve.",
 }
 
 // Projects Page 
 export const PROJECTS: Page = {
   TITLE: "Projects",
-  DESCRIPTION: "Recent projects I have worked on.",
+  DESCRIPTION: "Projects I have worked on.",
 }
 
 // Search Page
@@ -38,8 +37,8 @@ export const LINKS: Links = [
     HREF: "/", 
   },
   { 
-    TEXT: "Work", 
-    HREF: "/work", 
+    TEXT: "Resume",
+    HREF: "/resume",
   },
   { 
     TEXT: "Blog", 
@@ -56,26 +55,26 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "giyaseddin.bayrak@gmail.com",
+    HREF: "mailto:giyaseddin.bayrak@gmail.com",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "giyaseddin",
+    HREF: "https://github.com/giyaseddin"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+    TEXT: "giyaseddin",
+    HREF: "https://www.linkedin.com/in/giyaseddin/",
   },
   { 
     NAME: "Twitter",
     ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+    TEXT: "giyaseddinbyrk",
+    HREF: "https://x.com/giyaseddinbyrk",
   },
 ]
 
